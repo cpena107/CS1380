839f5bbdcd7d822bb693776a119b34952ffe826a
ANDRESPC\astar <andrescapc106@gmail.com>
2018-10-24 13:37:08 -0500

    cpp exercises

---
+++ CppExercises/examGradeCalculator.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	//step 0: variables and stuff
+
+	//step 1: ASk user how many exams they took
+
+	//step 2: Ask user to enter numExams grades, one by one, sum the total
+
+	//step 3: compute average total/numExams
+
+	//step 4:  tell user their grade and if they passed.
+
+
+	return 0;
+}
---
+++ CppExercises/whileLoops.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+	//while loops!
+
+	//example: infinite loop... this is probably a dumb idea.
+//	while ( 5 > 4 )
+	//{
+		//cout << "What will hapen here..." << endl;
+	//}
+
+
+	//challenge 1: print your name exactly 10 times
+
+	//challenge 2: print 1 2 3 4 5 6 7 ... 29 30
+
+	//challenge 3: Ask user to enter number, say 20.  Then countdown from that number to 1, 20 19 18.. 1 BLASTOFF!
+
+	//challenge 4: Ask user to enter a number n.  compute and print value of 1+2+3 + ... + n.
+	//Cool, can you do it with fewer variables?  Try it at home.
+
+
+	//Challenge 5: Ask user to ennter secret password (it's "blastfoof" ).  If they guess wrong,
+	//make keep entering until they guess correctly.
+	//For at home:  add a counter so it kicks you out if you guess more than 5 wrong passwords.
+
+
+	return 0;
+}
