8541c3064bd9b8b611ce66a7fc68ff8ceb2c60fc
astaroth106 <andrescapc@aol.com>
2018-10-31 00:28:01 -0500

    In Class Exercise

--- CppExamples/examGradeCalculator.cpp
+++ CppExamples/examGradeCalculator.cpp
@@ -1,48 +1,23 @@
-
-
 #include <iostream>
-#include <string>
+
 using namespace std;
 
-int main() 
+
+
+int main()
+
 {
-	//step 0: variables and stuff
-	int numExams;
-	double score;
-	double average;
 
-	//step 1: ASk user how many exams they took
-	cout << "HOw many exams did Schweller make you take this semester?" << endl;
-	cin >> numExams;
-	cout << "OUch!  " << numExams << " is pretty rough... " << endl;
+	// declaration of variables
 
-	//step 2: Ask user to enter numExams grades, one by one, sum the total
-	cout << "Please enter your exam grades one by one: " << endl;
+	// prompt for and read the lengths of the sides
 
-	int counter = 0;
-	double sum = 0;
-	while (counter < numExams)
-	{
-		cin >> score;
-		sum = sum + score;
-		counter = counter + 1;
-	}
+	// compute the semiperimeter and area
 
-	//step 3: compute average total/numExams
-	average = sum / numExams;
+	// display the input values and the resulting area
 
-	//step 4:  tell user their grade and if they passed.
-	cout << "Your average is: " << average << endl;
-
-	if (average >= 60)
-	{
-		cout << "You passed!" << endl;
-	}
-	else
-	{
-		cout << "See you next semester!" << endl;
-	}
 
 
 	return 0;
+
 }
---
+++ inClassAssignments/InClassAssignment.cpp
@@ -0,0 +1,46 @@
+/////////////////////////////////////////////////////////
+//
+//	Name:
+//	SID#
+//	CSCI 1380 Section ..
+//	Date:
+//
+//	This program simulates a magic eight ball
+//	it takes in a yes or no question from the user 
+//	and it answers with 8 possible options
+//	Yes, Most Likely, It is certain, Signs point to yes
+//	Cannot predict now, Ask again later, 
+//	No, Don't count on it.
+//
+//////////////////////////////////////////////////////////
+
+#include <iostream>
+#include<stdlib.h>
+#include<time.h>
+#include<string>
+//#include<fstream>
+
+using namespace std;
+
+int main() {
+
+	// Initialize your random seed
+	srand(time(NULL));
+
+	// step 1: Declare your variables
+	int answer;
+	string question;
+
+	// step 2: Ask the user to ask a yes/no question to the magic eight ball
+
+	// step 3: Using your random number generator and decide the answer to give the user
+	// HINT: answer = rand() % ? + 1
+
+	// step 4: Wrap your entire code (from step 2) in a while loop that after each question/answer
+	//			it will ask the user (y/n) if he/she wants to ask another question.
+
+	// BONUS: Save every question+answer in a file called "MyMagicEightBall.txt"
+
+	system("pause");
+	return 0;
+}
