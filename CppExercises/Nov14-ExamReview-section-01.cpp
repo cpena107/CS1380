c989e08c5e056683c3f50b32703a7ec96e5f70ea
Carlos Pena Caballero <andrescapc@aol.com>
2018-11-19 16:09:42 -0600

    Update Nov14-ExamReview-section-01.cpp

--- CppExercises/Nov14-ExamReview-section-01.cpp
+++ CppExercises/Nov14-ExamReview-section-01.cpp
@@ -3,7 +3,92 @@
 
 using namespace std;
 
+// Functions
+// examples of functions int main()
+// Return Type (int, string, double, char, bool)
+// Void is the only return type that does not return data
+// Name of the function
+// Parameters of the function
+
+// Declare your functions above main()
+// to have them defined inside main()
+
+// Basic function
+// returnType nameOfFunction (Param1, Param2, ... ){ // body}
+
+// Examples
+// Return Type is int
+// Function Name is PlusTwo
+// Parameter is num (int type)
+int PlusTwo(int num) {
+	return num + 2;
+}
+
+// Return type is string
+// Function Name is sayHello
+// Parameter are empty
+string sayHello() {
+	return "Hello";
+}
+
+// examples on double, char, bool
+// variables declared inside the function do not exist in main
+double getAverage(double grade, int num) {
+	double avg;
+	// so could also do cout's in here
+	avg = grade / num;
+	return avg;
+}
+
+// example on void
+void sayGoodbye() {
+	cout << 5 + 4 << endl;
+	cout << "Good Bye" << endl;
+}
+
+int PowerOf(int num, int power) {
+	int result = num; // assign num to result
+
+	for (int k = 1; k < power; k++) {
+		result *= num;
+	}
+
+	if (power == 0) {
+		return 1; // return terminates the function run
+	}
+	return result;
+}
+
+
 int main() {
+
+	// test PowerOf
+	cout << PowerOf(3, 3) << endl; // working good
+	cout << PowerOf(3, 0) << endl; 
+
+	// test sayGoodbye
+	sayGoodbye(); // void functions cannot be used as data 
+					// like the other functions
+
+	// test getAverage
+	cout << getAverage(187.50, 2) << endl;
+	
+	// call sayHello
+	string saySomething = sayHello(); // the string "Hello" 
+								// is stored in the variable
+								// saySomething
+
+	cout << saySomething << endl; // one way to do it
+	cout << sayHello() << " World" << endl; // another way
+
+	// Calling function in main
+	// call the function name and parameters
+	PlusTwo(8); // your code is running but the value from
+				// the function is lost
+	int number = PlusTwo(8); // the value returned from the
+							// function is stored in number
+							// int number = 8 + 2;
+
 	// Exam Review
 
 	// Variable declaration
@@ -18,6 +103,9 @@ int main() {
 	h = 'A'; // char variables are assigned with characters
 	// i = 'A'; // error
 	i = 10; // assignment for i to get the value 10
+
+	// assign values as you declare the variable
+	int myInt = 10;
 
 	// Arithmetic Operations
 	// +, -, *, /, %
@@ -46,19 +134,20 @@ int main() {
 	int newNum = 15;
 
 	cout << (num > newNum % 3) << endl; // 1
-	num += 50;
-	newNum /= 3;
+	num += 50; // stores 80 into num
+	newNum /= 3; // stores 5 into newNum
 	cout << (num % 5 == newNum % 3) << endl; // 0
 
 	// if-statements
 	// basic if-else tree
 	/*		(num % 5 == newNum % 3)
-		if (statement) {
+		if (condition) {
 			//body
 			// do your code inside the if
+			// ignore the rest of the if statement tree
 		}
 		// else is the default statement for the if
-		else {
+		else { 
 			//body
 			// do your code inside the else
 		}
@@ -78,6 +167,7 @@ int main() {
 	// regardless of the output in the first if
 	else if (grade >= 80) {
 		// output B
+		cout << "B" << endl;
 	}
 	// check if grade is greater than 70 for a 'C'
 	else if (grade >= 70) {
@@ -86,6 +176,7 @@ int main() {
 	else if (grade >= 60) {
 		// output D
 	}
+	//else if (grade < 60)
 	// check for F?
 	else {
 		// output F
@@ -97,7 +188,7 @@ int main() {
 	// a loop that you run until the condition is false
 	// basic while loop
 	/*
-		while ( statement) {
+		while (condition) {
 			// this is the body of the loop
 		}
 	*/
@@ -106,6 +197,7 @@ int main() {
 	// the value of the variables in the condition
 	// num starts at 80 and newNum starts at 15
 	newNum = 15;
+	// num is 80
 	int counter = 0;
 	while (num > newNum) {
 		cout << num << endl;
@@ -173,6 +265,15 @@ int main() {
 	// k++
 	// check if k (10) < 10 = false
 	// exit loop
+
+
+	// Functions
+	// examples of functions int main()
+	// Return Type (int, string, double, char, bool)
+	// Void is the only return type that does not return data
+	// Name of the function
+	// Parameters of the function
+
 
 	system("pause");
 	return 0;
